c7c49c886dbd3ad792170c5f8b182143a368a975
albert <albertprofe@gmail.com>
2025-09-23 11:10:55 +0200

    feat: list of fake cars

--- rentingCar/src/main/java/org/example/App.java
+++ rentingCar/src/main/java/org/example/App.java
@@ -1,13 +1,8 @@
 package org.example;
 
 import com.github.javafaker.Faker;
-
 import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -16,15 +11,14 @@ public class App
 
         // this is a static method
         // it can be called without creating an instance of the class
+
         //RentingCarTests.testCar();
         //RentingCarTests.testBooking();
 
         //RentingCarTests.testList();
-
         //RentingCarTests.TestFaker();
 
         CreateFakeCarList();
-
 
         System.out.println("Finished!");
     }
@@ -47,10 +41,34 @@ public class App
                     // add mycar to the list
                     ///  cars.add(mycar);
 
-
-
-
         // finish loop
+
+
+        ArrayList<Car> cars = new ArrayList<>();
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 100; i++) {
+            // create a car object
+            Car myCar = new Car();
+            // set fake data
+            myCar.setId( String.valueOf (  faker.number().randomNumber()));
+            myCar.setBrand(faker.company().name());
+            myCar.setYear(faker.number().numberBetween(1980, 2024));
+            // add mycar to the list
+            cars.add(myCar);
+        }
+
+        System.out.println("\n");
+        System.out.println("This is my list of cars: ");
+        System.out.println("-------------------------");
+        System.out.println("The list has " + cars.size() + " cars");
+        System.out.println("\n");
+        
+        // Print each car on a separate line
+        for (Car car : cars) {
+            System.out.println("\t" + car);
+        }
+        System.out.println("\n");
     }
 
 
