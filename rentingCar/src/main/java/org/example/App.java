e6c520859a509933a3439436953b4dde3a6fb93f
albert <albertprofe@gmail.com>
2025-09-25 12:57:12 +0200

    test: epoch

--- rentingCar/src/main/java/org/example/App.java
+++ rentingCar/src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
 package org.example;
-
-import com.github.javafaker.Faker;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App 
 {
@@ -31,8 +30,30 @@ public class App
         myDataStore.setActive(true);
 
         FakeDataDBPopulator.populateDBByCars(myDataStore);
+        createAndPrintEpoch();
+
+
 
         System.out.println("Finished!");
+    }
+
+
+    public static void createAndPrintEpoch() {
+
+        long epoch = System.currentTimeMillis()/1000;
+
+        System.out.println("Epoch 1: " + epoch);
+
+        String date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(epoch * 1000L));
+        System.out.println("Creation Date epoch1 to date: " + date);
+
+        try {
+            long epoch2 = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse("01/02/1972 01:00:00").getTime() / 1000L;
+            System.out.println("Epoch 2: " + epoch2);
+        } catch (java.text.ParseException e) {
+            System.err.println("Error parsing date: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
