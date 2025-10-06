c17cc35ca52a2edc52af4d7fb3116f5db7b2aab7
albert <albertprofe@gmail.com>
2025-10-06 11:49:42 +0200

    feat: spring boot project h2 db, entity, repository, test

--- rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
+++ rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
@@ -1,13 +1,31 @@
 package dev.app.rentingCar_boot;
 
+import dev.app.rentingCar_boot.model.Car;
+import dev.app.rentingCar_boot.repository.CarRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RentingCarBootApplicationTests {
 
+    @Autowired
+    CarRepository carRepository;
+
 	@Test
 	void contextLoads() {
 	}
+
+
+    @Test
+    void testCarRepository() {
+
+        //Car car = new Car("1", "Toyota", "Corolla", "123456", 2022, 100.0);
+        //carRepository.save(car);
+
+        Car car2 = new Car("3", "Toyota", "Corolla", "123456", 2022, 100.0);
+        carRepository.save(car2);
+
+    }
 
 }
