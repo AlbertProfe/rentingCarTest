45089eaae572f44a6f070ed8380825b68b3cda78
albert <albertprofe@gmail.com>
2025-10-07 08:15:32 +0200

    feat: v2.0 cars.html wiht @Controller

--- rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
+++ rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
@@ -20,11 +20,20 @@ class RentingCarBootApplicationTests {
     @Test
     void testCarRepository() {
 
-        //Car car = new Car("1", "Toyota", "Corolla", "123456", 2022, 100.0);
-        //carRepository.save(car);
+        Car car = new Car("1", "Toyota", "Corolla", "123456", 2022, 100.0);
+        carRepository.save(car);
 
-        Car car2 = new Car("3", "Toyota", "Corolla", "123456", 2022, 100.0);
-        carRepository.save(car2);
+        // Car car2 = new Car("2", "Toyota", "Corolla", "123456", 2022, 100.0);
+        // carRepository.save(car2);
+
+        // Car car3 = new Car("3", "Toyota", "Corolla", "123456", 2022, 100.0);
+        // carRepository.save(car3);
+
+        // Car car4 = new Car("4", "Toyota", "Corolla", "123456", 2022, 100.0);
+        // carRepository.save(car4);
+
+        Car car5 = new Car("5", "Toyota", "Corolla", "123456", 2022, 100.0);
+        carRepository.save(car5);
 
     }
 
