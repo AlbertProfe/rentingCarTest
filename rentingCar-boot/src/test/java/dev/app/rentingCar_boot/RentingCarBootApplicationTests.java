e6003237e71ca3d4b6912c8f6c5caaba1c2e59d0
albert <albertprofe@gmail.com>
2025-10-07 14:17:02 +0200

    feat: v2.2 serviceCar

--- rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
+++ rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
@@ -2,15 +2,21 @@ package dev.app.rentingCar_boot;
 
 import dev.app.rentingCar_boot.model.Car;
 import dev.app.rentingCar_boot.repository.CarRepository;
+import dev.app.rentingCar_boot.service.CarService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class RentingCarBootApplicationTests {
 
     @Autowired
     CarRepository carRepository;
+
+    @Autowired
+    CarService carService;
 
 	@Test
 	void contextLoads() {
@@ -35,6 +41,27 @@ class RentingCarBootApplicationTests {
         Car car5 = new Car("5", "Toyota", "Corolla", "123456", 2022, 100.0);
         carRepository.save(car5);
 
+    }
+
+
+    @Test
+    void tesFindCarById() {
+
+        String id = "10";
+
+        Optional<Car> car = carService.findCarById(id);
+
+
+
+    }
+
+
+    @Test
+    void testDeleteById(){
+
+        String id = "1";
+
+        carService.deleteCarById(id);
     }
 
 }
